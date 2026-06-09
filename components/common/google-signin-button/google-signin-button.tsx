5639eba492478e2adf56525088d9d5028d18a27a
Quanghoang059 <quanghoanglevi@gmail.com>
2026-06-09 21:56:38 +0700

    fix google login

--- components/common/google-signin-button/google-signin-button.tsx
+++ components/common/google-signin-button/google-signin-button.tsx
@@ -8,13 +8,24 @@ interface GoogleSignInButtonProps {
   onSuccess: (data: { email: string; firstName: string; lastName: string; photoUrl?: string }) => void;
   onError?: () => void;
 }
+const parseJwt = (token: string) => {
+  const base64Url = token.split('.')[1];
 
+  const base64 = base64Url
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+    .padEnd(Math.ceil(base64Url.length / 4) * 4, '=');
+
+  return JSON.parse(atob(base64));
+};
 export default function GoogleSignInButton({ onSuccess, onError }: GoogleSignInButtonProps) {
   const handleSuccess = (credentialResponse: CredentialResponse) => {
     if (!credentialResponse.credential) return;
 
     try {
-      const payload = JSON.parse(atob(credentialResponse.credential.split('.')[1]));
+      debugger
+      const payload = parseJwt(credentialResponse.credential);
+
       onSuccess({
         email: payload.email || '',
         firstName: payload.given_name || '',
